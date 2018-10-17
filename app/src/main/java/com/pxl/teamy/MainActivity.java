d0f1048bc0284f888876d03252b4170b67c3a3d5
denizalidogan <denizalidogan@hotmail.com>
2018-10-17 17:59:27 +0200

    Register and Settings Window
    Bug fixes.
    Register added.
    Settings window added.
    Firebase database added

--- app/src/main/java/com/pxl/teamy/MainActivity.java
+++ app/src/main/java/com/pxl/teamy/MainActivity.java
@@ -1,8 +1,8 @@
 package com.pxl.teamy;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
