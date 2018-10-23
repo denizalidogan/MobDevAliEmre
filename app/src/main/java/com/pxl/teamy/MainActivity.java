e514aa49d5290f13369d14bc65971845aa39b507
emre <ieozt38@gmail.com>
2018-10-23 20:08:06 +0200

    Bottom Navigation bar functions added/ back button top bar
    Bottom navigation bar added with some fragments. Top navigation bar back button added and some minor design fixes

--- app/src/main/java/com/pxl/teamy/MainActivity.java
+++ app/src/main/java/com/pxl/teamy/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -25,13 +27,11 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseFirestore firebaseFirestore;
     private String current_user_id;
-
     private FloatingActionButton btnAddPost;
-
     private BottomNavigationView mainBottemNav;
-
-
-
+    private HomeFragment homeFragment;
+    private NotificationFragment notificationFragment;
+    private AccountFragment accountFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,24 +40,46 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
-
-
-
-
         mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
 
         getSupportActionBar().setTitle("Teamy");
 
+        mainBottemNav = findViewById(R.id.mainBottomNav);
 
-        mainBottemNav = findViewById(R.id.mainBottemNav);
+        //FRAGMENTS
+        homeFragment = new HomeFragment();
+        notificationFragment = new NotificationFragment();
+        accountFragment = new AccountFragment();
+
+        mainBottemNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.bottom_action_home:
+                        replaceFragment(homeFragment);
+                        return true;
+                    case R.id.bottom_action_notification:
+                        replaceFragment(notificationFragment);
+                        return true;
+                    case R.id.bottom_action_myaccount:
+                        replaceFragment(accountFragment);
+                        return true;
+
+                    case R.id.bottom_action_add:
+                        sentToAddEvent();
+                        return true;
+
+                        default:
+                            return false;
+                }
+            }
+        });
 
         btnAddPost = findViewById(R.id.btnAddPost);
-
         btnAddPost.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-
                 Intent newPostIntent = new Intent(MainActivity.this    , NewPostActivity.class);
                 startActivity(newPostIntent);
             }
@@ -65,27 +87,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     @Override
     protected void onStart() {
         super.onStart();
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser == null) {
 
-
-
             sentToLogin();
         } else{
             //Checken of de gebruiker wel foto enz heeft.
-
             current_user_id = mAuth.getCurrentUser().getUid();
 
             firebaseFirestore.collection("Users").document(current_user_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-
                     if(task.isSuccessful()){
-
                         if(!task.getResult().exists()){
 
                             Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
@@ -99,10 +115,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
-
         }
     }
-
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
@@ -111,30 +125,30 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
             case R.id.action_btnLogout:
                 logOut();
                 return true;
-
-
             case R.id.action_btnSettings:
-
                 Intent settingsIntent = new Intent(MainActivity.this, SetupActivity.class);
                 startActivity(settingsIntent);
-
                 return true;
-
             default:
                 return false;
-
         }
-
     }
 
     private void logOut() {
         mAuth.signOut();
         sentToLogin();
+    }
+
+
+    private void sentToAddEvent() {
+        // Activiteit wordt aangemaakt
+        startActivity(new Intent(this, NewPostActivity.class));
+        // Zorgt ervoor dat je niet terug kunt met bv Back button
+        finish();
     }
 
 
@@ -144,5 +158,9 @@ public class MainActivity extends AppCompatActivity {
         // Zorgt ervoor dat je niet terug kunt met bv Back button
         finish();
     }
-
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction =  getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, fragment);
+        fragmentTransaction.commit();
+    }
 }
