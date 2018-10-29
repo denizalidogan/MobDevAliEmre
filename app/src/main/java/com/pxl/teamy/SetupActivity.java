c2e905350e85a57b5b0d8d5fd8e8ce057bb4b347
denizalidogan <denizalidogan@hotmail.com>
2018-10-29 14:02:40 +0100

    Setup Activity
    Profile settings More info added
    Gender added
    username added
    bio added
    datepicker added
    date of birth added
    few changes layout

--- app/src/main/java/com/pxl/teamy/SetupActivity.java
+++ app/src/main/java/com/pxl/teamy/SetupActivity.java
@@ -1,9 +1,12 @@
 package com.pxl.teamy;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,10 +16,15 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -32,6 +40,8 @@ import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -49,6 +59,16 @@ public class SetupActivity extends AppCompatActivity {
     private EditText setupName;
     private Button setupBtn;
     private ProgressBar setupProgress;
+    private EditText setupUsername;
+    private TextView setupBirth;
+    private EditText setupBio;
+
+    private RadioGroup rg;
+    private RadioButton rb;
+    private String gender;
+
+    private static final String TAG = "SetupActivity";
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
 
     private StorageReference storageReference;
     private FirebaseAuth firebaseAuth;
@@ -75,13 +95,60 @@ public class SetupActivity extends AppCompatActivity {
         setupName = findViewById(R.id.setup_name);
         setupBtn = findViewById(R.id.setup_btn);
         setupProgress = findViewById(R.id.setup_progress);
+        setupUsername = findViewById(R.id.setup_username);
+        setupBirth = findViewById(R.id.setup_birth);
+        setupBio = findViewById(R.id.setup_bio);
+        rg = findViewById(R.id.setup_gendergroup);
 
         setupProgress.setVisibility(View.VISIBLE);
         setupBtn.setEnabled(false);
 
+        setupBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
 
+                DatePickerDialog dialog = new DatePickerDialog(
+                        SetupActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        onDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
 
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + dayOfMonth + "/" + year);
 
+                String date = dayOfMonth + "/" + month + "/" + year;
+                setupBirth.setText(date);
+            }
+        };
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                rb = rg.findViewById(checkedId);
+
+                switch(checkedId){
+                    case R.id.setup_gendermale:
+                        gender = rb.getText().toString();
+                        break;
+                    case R.id.setup_genderfemale:
+                        gender = rb.getText().toString();
+                        break;
+
+                    default:
+                }
+            }
+        });
 
 
         //Glide gebruikt om passief foto en naam te kunnen opladen
@@ -90,16 +157,38 @@ public class SetupActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
 
 
-//Does the colllection excist?
+                //Does the colllection excist?
                 if(task.isSuccessful()){
-// User exist?
+                    // User exist?
                     if(task.getResult().exists()){
 
                         String name = task.getResult().getString("name");
                         String image = task.getResult().getString("image");
+                        String username = task.getResult().getString("username");
+                        String date = task.getResult().getString("dateOfBirth");
+                        String bio = task.getResult().getString("bio");
+                        String genderStr = task.getResult().getString("gender");
+
 
                         mainImageURI = Uri.parse(image);
                         setupName.setText(name);
+                        setupUsername.setText(username);
+                        setupBirth.setText(date);
+                        setupBio.setText(bio);
+
+                        switch(genderStr){
+                            case "Male":
+                                rb = findViewById(R.id.setup_gendermale);
+                                rb.setChecked(true);
+                                break;
+                            case "Female":
+                                rb = findViewById(R.id.setup_genderfemale);
+                                rb.setChecked(true);
+                                break;
+
+                            default:
+                        }
+
                         RequestOptions placeholderRequest = new RequestOptions();
                         placeholderRequest.placeholder(R.drawable.default_image);
 
@@ -122,6 +211,10 @@ public class SetupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 final String user_name = setupName.getText().toString();
+                final String user_username = setupUsername.getText().toString();
+                final String user_date = setupBirth.getText().toString();
+                final String user_bio = setupBio.getText().toString();
+                final String user_gender = rb.getText().toString();
 
                 if (!TextUtils.isEmpty(user_name) && mainImageURI != null) {
 
@@ -138,7 +231,7 @@ public class SetupActivity extends AppCompatActivity {
                             public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
 
                                 if (task.isSuccessful()) {
-                                    storeFirestore(task, user_name);
+                                    storeFirestore(task, user_name, user_username, user_date, user_bio, user_gender);
                                 } else {
 
                                     String error = task.getException().getMessage();
@@ -148,7 +241,7 @@ public class SetupActivity extends AppCompatActivity {
                             }
                         });
                     } else {
-                        storeFirestore(null, user_name);
+                        storeFirestore(null, user_name, user_username, user_date, user_bio, user_gender);
                     }
                 }
             }
@@ -178,7 +271,7 @@ public class SetupActivity extends AppCompatActivity {
 
 
     //hasmap uploaden naar firestore voor de referenties te kunnen legen met de gebruikers
-    private void storeFirestore(@NonNull Task<UploadTask.TaskSnapshot> task, String user_name) {
+    private void storeFirestore(@NonNull Task<UploadTask.TaskSnapshot> task, String user_name, String user_username, String user_date, String user_bio, String user_gender) {
 
         Uri download_uri;
 
@@ -192,6 +285,10 @@ public class SetupActivity extends AppCompatActivity {
         Map<String, String> userMap = new HashMap<>();
         userMap.put("name", user_name);
         userMap.put("image", download_uri.toString());
+        userMap.put("username", user_username);
+        userMap.put("dateOfBirth", user_date);
+        userMap.put("bio", user_bio);
+        userMap.put("gender", user_gender);
 
         firebaseFirestore.collection("Users").document(user_id).set(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
@@ -236,5 +333,10 @@ public class SetupActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    public void genderClick(View view){
+        int radiobuttonid = rg.getCheckedRadioButtonId();
+        rb =  findViewById(radiobuttonid);
     }
 }
