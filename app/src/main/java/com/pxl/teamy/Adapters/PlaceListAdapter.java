ce7f06bc460aba258761fa19fdcc91e0f55c12ae
emre <ieozt38@gmail.com>
2018-11-05 18:47:38 +0100

    Added google maps for choosing LOC/
    Remembering locs in MobileDB

---
+++ app/src/main/java/com/pxl/teamy/Adapters/PlaceListAdapter.java
@@ -0,0 +1,147 @@
+package com.pxl.teamy.Adapters;
+
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.pxl.teamy.R;
+import com.pxl.teamy.ViewActivities.DetailActivity;
+
+public class PlaceListAdapter extends RecyclerView.Adapter<PlaceListAdapter.PlaceViewHolder> {
+
+    private Context mContext;
+    private PlaceBuffer mPlaces;
+
+
+
+
+    public PlaceListAdapter(Context context, PlaceBuffer places) {
+        this.mContext = context;
+        this.mPlaces = places;
+    }
+
+
+
+
+
+    @Override
+    public PlaceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Get the RecyclerView item layout
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.item_place_card, parent, false);
+        return new PlaceViewHolder(view);
+    }
+
+
+    public String placeAddress;
+    @Override
+    public void onBindViewHolder(final PlaceViewHolder holder, final int position) {
+        String placeName = mPlaces.get(position).getName().toString();
+         placeAddress = mPlaces.get(position).getAddress().toString();
+        holder.nameTextView.setText(placeName);
+        holder.addressTextView.setText(placeAddress);
+
+
+
+
+
+        holder.post_locmaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(context, DetailActivity.class);
+//                intent.putExtra("EVENT_POST_ID", eventPostId);
+//                context.startActivity(intent);
+
+                notifyDataSetChanged();
+
+                holder.post_locmaps.setBackgroundColor(Color.CYAN);
+
+
+
+
+
+
+            }
+        });
+
+
+
+
+
+
+    }
+
+    public void swapPlaces(PlaceBuffer newPlaces){
+        mPlaces = newPlaces;
+        if (mPlaces != null) {
+            // Force the RecyclerView to refresh
+            this.notifyDataSetChanged();
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        if(mPlaces==null) return 0;
+        return mPlaces.getCount();
+    }
+
+
+
+    public String getPlaats() {
+      return placeAddress;
+    }
+
+
+
+    /**
+     * PlaceViewHolder class for the recycler view item
+     */
+    class PlaceViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameTextView;
+        TextView addressTextView;
+        private LinearLayout post_locmaps;
+
+        public PlaceViewHolder(View itemView) {
+            super(itemView);
+
+            post_locmaps = itemView.findViewById(R.id.post_locmaps);
+
+
+
+            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
+            addressTextView = (TextView) itemView.findViewById(R.id.address_text_view);
+
+
+        }
+
+
+
+    }
+}
