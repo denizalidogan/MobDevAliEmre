8391d2492ab108dd7d422c6c3d2a97d64a9e978d
denizalidogan <denizalidogan@hotmail.com>
2018-11-09 02:00:24 +0100

    Landscape fixed

--- app/src/main/java/com/pxl/teamy/DomainClasses/Statics.java
+++ app/src/main/java/com/pxl/teamy/DomainClasses/Statics.java
@@ -4,6 +4,8 @@ public  final class  Statics {
 
     private static boolean  isLandscape ;
 
+    private static String eventId;
+
     public static boolean isIsLandscape() {
         return isLandscape;
     }
@@ -11,6 +13,16 @@ public  final class  Statics {
     public static void setIsLandscape(boolean isLandscape) {
         Statics.isLandscape = isLandscape;
     }
+
+
+    public static String getEventId() {
+        return eventId;
+    }
+
+    public static void setEventId(String eventId) {
+        Statics.eventId = eventId;
+    }
+
 
 
 
